5ff60843f8e32e88296b04633269455b0e4979c8
Caregraf <info@caregraf.com>
2013-07-26 09:37:31 -0700

    deleted dummy fms

--- Framework/fmsDescribeResult.py
+++
@@ -1,8 +0,0 @@
-class FMSDescribeReply(object):
-
-    """
-    A peer of (FMQL)DescribeReply, this allows you to easily process equivalent data stored as RDF in a triple store
-    
-    Note: big differences - natively SPARQL doesn't have the notion of "context" ie/ qualifying (often provenance) data or that all URIs have rdfs:labels.  
-    """
-    pass
