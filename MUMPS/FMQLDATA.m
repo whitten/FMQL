cc58288728f1697c43bb598b150ac4eca11fe74d
conorcaregraf <conor-dowling@caregraf.com>
2014-03-19 18:05:37 +0000

    small mumps fixes

--- MUMPS/FMQLDATA.m
+++ MUMPS/FMQLDATA.m
@@ -194,6 +194,7 @@ ONEFIELD(FAR,ID,FDINF) ;
  . N LOCSUB S LOCSUB=@FAR@(ID,FDINF("LOCSUB"))
  . ; For $E values, don't just take the $E limit.
  . N IVALUE S IVALUE=$S($D(FDINF("LOCPOS")):$P(LOCSUB,"^",FDINF("LOCPOS")),1:LOCSUB) Q:IVALUE=""
+ . ; Note: issue with EVALUE here before type specific checks is that EVAL must dup the checks
  . N EVALUE S EVALUE=$$GETEVAL^FMQLUTIL(.FDINF,IVALUE)
  . I FDINF("TYPE")=7 D
  . . N PFLINF D BLDFLINF^FMQLUTIL(FDINF("PFILE"),.PFLINF)
@@ -204,7 +205,6 @@ ONEFIELD(FAR,ID,FDINF) ;
  . . D ASSERT^FMQLJSON(REPLY,FDINF("LABEL"),FDINF("FIELD"),"7",PVALUE,PLABEL,.PSAMEAS)
  . E  I FDINF("TYPE")=8 D
  . . N PID S PID=$P(IVALUE,";")
- . . Q:PID=""  ; Corrupt pointer
  . . Q:PID'=+PID  ; Corrupt pointer
  . . Q:$P(IVALUE,";",2)=""  ; Corrupt pointer
  . . N LOCZ S LOCZ="^"_$P(IVALUE,";",2)_"0)"  ; 0 has file's description
