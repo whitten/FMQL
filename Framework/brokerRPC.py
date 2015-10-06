00d5d0ea5dc4709030d292d1ff429e3b2f3052f0
caregraf <info@caregraf.com>
2015-10-05 22:20:33 -0700

    fixed - temp - need a switch - brokerRPC to work with OSEHRA's new cypher

--- Framework/brokerRPC.py
+++ Framework/brokerRPC.py
@@ -40,9 +40,11 @@ from random import randint
 class RPCConnection(object):
 
     """
-    Hardcoded in VistA/RPMS access code.
+    Hardcoded in VistA/RPMS access code: in XUSRB1.m
+
+    TMP: support OSEHRA's changed Cypher. See: https://www.osehra.org/blog/vista-m-and-delphi-guis-problem
     """
-    CIPHER = [
+    CIPHER_OLD = [
         "wkEo-ZJt!dG)49K{nX1BS$vH<&:Myf*>Ae0jQW=;|#PsO`'%+rmb[gpqN,l6/hFC@DcUa ]z~R}\"V\\iIxu?872.(TYL5_3",
         "rKv`R;M/9BqAF%&tSs#Vh)dO1DZP> *fX'u[.4lY=-mg_ci802N7LTG<]!CWo:3?{+,5Q}(@jaExn$~p\\IyHwzU\"|k6Jeb",
         "\\pV(ZJk\"WQmCn!Y,y@1d+~8s?[lNMxgHEt=uw|X:qSLjAI*}6zoF{T3#;ca)/h5%`P4$r]G'9e2if_>UDKb7<v0&- RBO.",
@@ -63,6 +65,28 @@ class RPCConnection(object):
         "~A*>9 WidFN,1KsmwQ)GJM{I4:C%}#Ep(?HB/r;t.&U8o|l['Lg\"2hRDyZ5`nbf]qjc0!zS-TkYO<_=76a\\X@$Pe3+xVvu",
         "yYgjf\"5VdHc#uA,W1i+v'6|@pr{n;DJ!8(btPGaQM.LT3oe?NB/&9>Z`-}02*%x<7lsqz4OS ~E$\\R]KI[:UwC_=h)kXmF",
         "5:iar.{YU7mBZR@-K|2 \"+~`M%8sq4JhPo<_X\\Sg3WC;Tuxz,fvEQ1p9=w}FAI&j/keD0c?)LN6OHV]lGy'$*>nd[(tb!#"]
+
+    CIPHER = [
+        "VEB_0|=f3Y}m<5i$`W>znGA7P:O%H69[2r)jKh@uo\\wMb*Da !+T?q4-JI#d;8ypUQ]g\"~'&Cc.LNt/kX,e{vl1FRZs(xS",
+        "D/Jg><p]1W6Rtqr.QYo8TBEMK-aAIyO(xG7lPz;=d)N}2F!U ,e0~$fk\"j[m*3s5@XnZShv+`b'{u&_\\9%|wL4ic:V?H#C",
+        "?lBUvZq\\fwk+u#:50`SOF9,dp&*G-M=;{8Ai6/N7]bQ1szC!(PxW_YV~)3Lm.EIXD2aT|hKj$rnR@[\"c g'<>t%4oJHy}e",
+        "MH,t9K%TwA17-Bzy+XJU?<>4mo @=6:Ipfnx/Y}R8Q\\aN~{)VjEW;|Sq]rl[0uLFd`g5Z#e!3$b\"P_.si&G(2'Cvkc*ODh",
+        "vMy>\"X?bSLCl)'jhzHJk.fVc6#*[0OuP@\\{,&r(`Es:K!7wi$5F; DoY=p%e<t}4TQA2_W9adR]gNBG1~nIZ+3x-Um|8q/",
+        ":\"XczmHx;oA%+vR$Mtr CBTU_w<uEK5f,SW*d8OaFGh]j'{7-~Qp#yqP>09si|VY1J!/[lN23&L4`=.D6)ZIb\\n?}(ek@g",
+        "j7Qh[YU.u6~xm<`vfe%_g-MRF(#iK=trl}C)>GEDN *$OdHzBA98aLJ|2WP:@ko0wy4I/S&,q']5!13XcVs\\?Zp\"+{;Tbn",
+        "\\UVZ;.&]%7fGq`*SA=Kv/-Xr1OBHiwhP5ukYo{2\"}d |NsT,>!x6y~cz[C)pe8m9LaRI(MEFlt:Qg#D'n$W04b@_+?j<3J",
+        "MgSvV\"U'dj5Yf6K*W)/:z$oi7GJ|t(1Ak=ZC,@]Q0?8DnbE[+L`{mq>;aOR}wcB4sF_e9rh2l\\x<. PyNpu%IT!&3#HX~-",
+        "rFkn4Z0cH7)`6Xq|yL #wmuW?Gf!2YES;.B_D=el}hN[M&x(*AasU9otd+{]g>TQjp<:v%5O\"zI\\@$Rb~8i-3/'V1,CJPK",
+        "\\'%u+W)mK41L#:A6!;7(\"tnyRlaOe09]3EFd ITf.`@P[Q{B$_iYhZo*kbc|HUgz=D>Svr8x,X~-<NsjM}C/&J?p2wV5qG",
+        "QCl_329e+DTp&\\?jNys V]k*M\"X!$Y6[i@g>{RvF'01(45LJZU,:-uAwtB;7|%fx.n`IhSE<OoW~=bdP#/KHzrc)8mG}aq",
+        "!{w*PR[B9Oli~T, rFc\"/?ast8=)-_Dgo<E#n4HYA%f'N;0@S7pJ`kGIedM|+C2yjvL5b3K6\\Z]V(.h}umxz>XQ$qUW:1&",
+        "}:SHZ|O~A-bcyJ4%'5vM+ ;eo.$B)Vp\\,kTDz1sGL`]*=mg2nxYPd&lErN3[8qF0@u\"a_>wQKI{f6C7?9RX(t#i/U<j!Wh",
+        ",ry*|7<1keO:Wi C/zh4IZ>x!F[_(\"Dbu%Hl5Pg=]QG.LKcJ0&ont@+{;ATX6jMwBv?2#f`q\\}VYm'8Es$NpU)dR~S9a3-",
+        "h,=/:pJ$@mlY-`bwQ)e3Xt8.RUSMV 2A;j[PN}TE9x~kL&<ns5q>_#c1%K+rIuFoa(zyDWdH]?\\GB0g*4f6\"Z!'v{7|OiC",
+        "/$*b.ts0vOx_-o\"l3MHI~}!E`eJimPd>Sn&wzFUh?Kf4)g5X<,8pD:9LA{a[k;'|GyYQ=R2B\\#q+cru6N1W@(C TV]7Z%j",
+        "qEoC?YWNtV{Brg,I(i:e7Jd#6m!D8XT\"n[$~1*ZcxL.Kh2s4%Q&ju\\5Gvazw+9pF@k`HA)=U3/< -}'0b;|PfSRl_MO]y>",
+        "`@X:!R[\\tY5OBcZPh$rM_a-\"vgJG%|}oIH)wWQ*jDVxlp,'+S zu(&7?>KCn4y1dE02q6b<;F=8]9NAmT{Li3f/esUk.~#",
+        "\\Zr';/SMsG76Lj$aBc[#k>u=_O@2J&X{Aft xV4~vz8Q}q)0K.NIpRnYwDhg+<\"H-!(PF:m*]?,WCT|dE9o53%`liUey1b"]
 
     def __init__(self, host, port, access, verify, context, logger, endMark, poolId):
         """
@@ -487,7 +511,7 @@ def main():
 
     # VERY BASIC:
     connection = VistARPCConnection(args[0], int(args[1]), args[2], args[3], "CG FMQL QP USER", RPCLogger())
-    reply = connection.invokeRPC("CG FMQL QP", ["DESCRIBE 2-9"])
+    reply = connection.invokeRPC("CG FMQL QP", ["SELECT 2 LIMIT 100"])
     try:
         json.loads(reply)
     except:
@@ -495,7 +519,8 @@ def main():
         print reply
         print "... exiting"
         return
-    print reply[0:31]
+    print reply
+    return
 
     # 10 and 20 ie. pool size 10, request number 20. Can interplay. Should see some connection come more to the fore.
     # Should see, full size isn't 
